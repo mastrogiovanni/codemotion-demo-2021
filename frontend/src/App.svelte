6ce0205e629e8864180cfaa8821787bdcb9c5bf1
Michele Mastrogiovanni <michele.mastrogiovanni@gmail.com>
2021-08-28 11:39:37 +0200

    Added frontend and deployment files

---
+++ frontend/src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+	export let name;
+
+	async function click() {
+		console.log("click")
+	}
+
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+
+	<button on:click={click} class="btn btn-lg btn-primary">Stress Test</button>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
